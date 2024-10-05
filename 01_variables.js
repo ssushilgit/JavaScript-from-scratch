d1b9bc832449ba628b2ccb33870d11ea1fa062e2
ssushilgit <shresthasushil741@gmail.com>
2024-10-06 00:38:41 +0545

    01_variables

--- 01_variables.js
+++ 01_variables.js
@@ -8,3 +8,7 @@ accountEmail = "sugamsth11@gmail.com"
 accountPassword = "567890"  
 accountCity = "Kathmandu"
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Prefer not to use var for variable declaration due to issues in scope. Instead use let and const.
+// let - let is blocke-scoped, meaning the variable is only accesible within the block it is defined(eg, for loop, if statement)
+// var - var on the other hand is a function-scoped or globally-scoped, meaning it is accesible throughout the funcion or globally if declared outside the function 
