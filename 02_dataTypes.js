ca468de785e9c13e24b74fa2029f10a1abbb9c15
ssushilgit <shresthasushil741@gmail.com>
2024-10-06 01:17:10 +0545

    02_dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 2 ) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code reusability should be high
+
+let name = "Sushil" //string
+let age = 22 // number
+let isLoggedIn = false //boolean
+let city //undefined
+
+console.log(typeof(age))
+
+// JavaScript has several primitive data types:
+
+// String: Defined using single or double quotes. Example: let name = "John"; 
+// Number: Represents both integers and floating-point numbers. Example: let age = 30; 
+// Boolean: Represents true or false values. Example: let isLoggedIn = true; 
+// Null: A special type representing an intentional absence of any value. Null is a stand-alone value
+// Undefined: Indicates a variable that has been declared but not assigned a value 
+// Symbol: A unique and immutable primitive value often used to identify object properties 
+
+console.log(typeof null)  // object
+console.log(typeof undefined) //undefined
