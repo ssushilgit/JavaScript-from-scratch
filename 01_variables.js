debbf8da8430c7500135337c2ebf32f3482b8bb4
ssushilgit <shresthasushil741@gmail.com>
2024-10-06 00:32:43 +0545

    01_variables

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1;
+let accountEmail = "shresthasushil741@gmail.com"
+var accountPassword = "123456"
+accountCity = "Damauli"
+let accountState;
+
+accountEmail = "sugamsth11@gmail.com"
+accountPassword = "567890"  
+accountCity = "Kathmandu"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
