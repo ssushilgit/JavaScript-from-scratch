ca79044f13b223ae99d10b53cfdd61fce79bd453
ssushilgit <shresthasushil741@gmail.com>
2024-10-17 22:42:37 +0545

    18_global and local scope

---
+++ 18_scopes.js
@@ -0,0 +1,10 @@
+ let a  = 1000
+ if(true){
+    let a =10
+    const b= 20
+    var c = 30
+    console.log("Inner value : ", a)
+ }
+ console.log("Outer value : ", a)
+//  console.log(b)
+ console.log(c)
