ff1b3fa08a990c7949656d2a214fca49a3119b8b
ssushilgit <shresthasushil741@gmail.com>
2024-10-21 00:27:48 +0545

    25_while, do while

---
+++ 25_while_dowhile.js
@@ -0,0 +1,30 @@
+// while
+
+let i=1
+while (i <= 10) {
+    // console.log(`value of i is ${i}`)
+    i = i+2;
+}
+
+myArray = ["Cricket", "Football", "PUBG"]
+
+let arr = 0 
+while (arr < myArray.length) {   // while(0<3)
+    // console.log(`Games are ${myArray[arr]}`)    
+    arr++;
+}
+
+// do while loop
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score<=10);
+
+
+
+let array = 0 
+do{
+    console.log(`Games are ${myArray[array]}`)    
+    array++;
+}while(array < myArray.length)  // while(0<3)
