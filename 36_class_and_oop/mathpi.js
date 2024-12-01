2966e26d566e2065835ced51452663ad5e55cf27
ssushilgit <shresthasushil741@gmail.com>
2024-12-01 23:41:48 +0545

    Object

---
+++ 36_class_and_oop/mathpi.js
@@ -0,0 +1,25 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+// console.log(descriptor)
+
+const chai = {
+    name : 'lemon tea',
+    price : 200,
+    isAvailable : true,
+
+    orderTea : function(){
+        console.log('Tea not ready')
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+Object.defineProperty(chai, "name" , {
+    // writable : false,
+    enumerable : false
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"))
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} : ${value}`)
+    }
+}
