b496214ee7fb3579a1f2126ad3a3d4164bdb2137
ssushilgit <shresthasushil741@gmail.com>
2024-12-03 01:28:14 +0545

    Getter and Setter

---
+++ 36_class_and_oop/object_getset.js
@@ -0,0 +1,18 @@
+// object based
+
+const User = {
+    _email : 's@gmail.com',
+    _password : 'abc',
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+
+const tea = Object.create(User)
+console.log(tea.email)
